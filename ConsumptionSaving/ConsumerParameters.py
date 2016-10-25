4c0b014ded0856012a6da8831d7be19dad356cd9
Christopher Carroll <github@llorracc.org>
2016-10-25 14:03:39 -0400

    Improve comments in ConsumerParameters

--- ConsumptionSaving/ConsumerParameters.py
+++ ConsumptionSaving/ConsumerParameters.py
@@ -1,6 +1,6 @@
 '''
 Specifies examples of the full set of parameters required to solve various
-consumption-saving models.  These models can be found in ConsIndShockModel,
+consumption-saving models.  Such models begin with "Cons", e.g. ConsIndShockModel,
 ConsAggShockModel, ConsPrefShockModel, and ConsMarkovModel.
 '''
 from copy import copy
